be76f54a148db325c69001e7978d78f097fb478f
Poojith-007 <gudavallipujith@gmail.com>
2026-09-23 10:02:13 +0530

    changed credientials in config

--- biteback/config.php
+++ biteback/config.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "biteback";
+$host = "sql304.infinityfree.com";
+$user = "if0_42985208";
+$pass = "ni1kh2il3";
+$db = "if0_4295208_biteback";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
